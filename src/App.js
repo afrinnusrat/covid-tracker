a98738bcaee77ee5312d47887497969e71717d8f
wildenali <wildenwildenaliali@gmail.com>
2020-09-04 10:38:03 +0700

    Install @material-ui/core

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import "./App.css";
 function App() {
   return (
     <div className="app">
+      {/* Header */}
       <h1>Covid-19 Tracker</h1>
 
-      {/* Header */}
       {/* Title + Select Input dropdow field */}
 
       {/* InfoBoxs */}
