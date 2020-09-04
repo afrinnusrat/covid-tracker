379e68f70ebb4e424f8931b54030118abbeff3fe
wildenali <wildenwildenaliali@gmail.com>
2020-09-04 14:38:49 +0700

    Bikin bagian kiri dan bagian kanan untuk penempatan komponen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from "react";
-import { MenuItem, FormControl, Select } from "@material-ui/core";
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 import "./App.css";
 import InfoBox from "./InfoBox";
 import Map from "./Map";
@@ -35,37 +41,49 @@ function App() {
 
   return (
     <div className="app">
-      {/* Header */}
-      <div className="app__header">
-        <h1>Covid-19 Tracker</h1>
-        {/* Title + Select Input dropdow field */}
-        <FormControl className="app__dropdown">
-          <Select variant="outlined" onChange={onCountryChange} value={country}>
-            <MenuItem value="worldwide">Worldwide</MenuItem>
-            {/* Loop through all the countries and show a drop down list of the options */}
-            {countries.map((country) => (
-              <MenuItem value={country.value}>{country.name}</MenuItem>
-            ))}
-          </Select>
-        </FormControl>
+      <div className="app__left">
+        {/* Header */}
+        <div className="app__header">
+          <h1>Covid-19 Tracker</h1>
+          {/* Title + Select Input dropdow field */}
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              onChange={onCountryChange}
+              value={country}
+            >
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {/* Loop through all the countries and show a drop down list of the options */}
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          {/* InfoBoxs title="Coronavirus cases" */}
+          <InfoBox title="Coronavirus Cases" cases={1110} total={2000} />
+
+          {/* InfoBoxs  title="Coronavirus recoveries" */}
+          <InfoBox title="Recovered" cases={1112} total={1500} />
+
+          {/* InfoBoxs */}
+          <InfoBox title="Death" cases={1115} total={500} />
+        </div>
+
+        {/* Map */}
+        <Map />
       </div>
 
-      <div className="app__stats">
-        {/* InfoBoxs title="Coronavirus cases" */}
-        <InfoBox title="Coronavirus Cases" cases={1110} total={2000} />
-
-        {/* InfoBoxs  title="Coronavirus recoveries" */}
-        <InfoBox title="Recovered" cases={1112} total={1500} />
-
-        {/* InfoBoxs */}
-        <InfoBox title="Death" cases={1115} total={500} />
-      </div>
-
-      {/* Map */}
-      <Map />
-
-      {/* Table */}
-      {/* Graph */}
+      <Card className="app__right">
+        <CardContent>
+          {/* Table */}
+          <h3>Live Cases by Country</h3>
+          {/* Graph */}
+          <h3>Live Cases by Country</h3>
+        </CardContent>
+      </Card>
     </div>
   );
 }
