76ba2128f31fe492121dfa85b072dfe16d98af0c
wildenali <wildenwildenaliali@gmail.com>
2020-09-04 10:41:24 +0700

    Membuat title and dropdown untuk header

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
 import "./App.css";
 
 function App() {
   return (
     <div className="app">
       {/* Header */}
-      <h1>Covid-19 Tracker</h1>
-
-      {/* Title + Select Input dropdow field */}
+      <div className="app__header">
+        <h1>Covid-19 Tracker</h1>
+        {/* Title + Select Input dropdow field */}
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            <MenuItem value="z">Worldwide</MenuItem>
+            <MenuItem value="a">Option 2</MenuItem>
+            <MenuItem value="b">Option 3</MenuItem>
+            <MenuItem value="c">Yeeeee</MenuItem>
+          </Select>
+        </FormControl>
+      </div>
 
       {/* InfoBoxs */}
       {/* InfoBoxs */}
